e7442a14bac8b908b2ab596af5ee332242cd01d9
Rajeev Sharma <kr.rajeevsharma@gmail.com>
2017-04-24 12:09:38 +0530

    added primery project files

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,62 +1,132 @@
 //1. The wrapper function
-module.exports = function(grunt) { 
+module.exports = function(grunt) {
 
-	//2. Project configuration
-	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
-		
-		//Define assemble task
-		assemble: {
+    //2. Project configuration
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
 
+        //Define all grunt task
+        assemble: {
             // Task-level options
             options: {
                 flatten: true,
-                data: ['assemble/data/**/*.{json,yml}'],
-                helpers: 'assemble/helpers/*.js',                
+                data: ['app/assemble/data/**/*.{json,yml}'],
+                //helpers: 'assemble/helpers/*.js',
                 // layouts are included in partials to allow recursive binding
-                partials: ['assemble/partials/*.hbs']
+                partials: ['app/assemble/partials/**/*.hbs']
             },
             site: {
-                options: { layoutdir: 'assemble/layouts/site', layout: 'default.hbs' },
-                files: [
-                    { expand: true, cwd: 'assemble/pages/site/', src: ['**/*.hbs'], dest: 'WebContent/site/' }
-                ]
+                options: {
+                    layoutdir: 'app/assemble/',
+                    layout: 'default.hbs'
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'app/assemble/pages/',
+                    src: ['**/*.hbs'],
+                    dest: 'WebContent'
+                }]
             }
         },
-		
-		connect: {
-            server: {
-                options: {
-                    port: 9000, // The port on which the webserver will respond.
-                    hostname: '*', // Default 'localhost'. Setting this to '*' will make the server accessible through IP. Useful for cross-device testing.
-                    livereload: 1338,
-                    base: 'app',					
-					keepalive: true
+        sass: {
+            options: {
+                sourcemap: 'none'
+            },
+            dist: {
+                files: [{
+                    expand: true,
+                    cwd: 'app/sass/',
+                    src: ['*.scss', '**/*.scss'],
+                    dest: 'WebContent/css',
+                    ext: '.css'
+                }]
+            }
+        },
+
+        browserSync: {
+            bsFiles: {
+                src: ['app/assemble/**/*.hbs',
+                    'app/sass/**/*.scss', 'app/js/*.js'
+                ]
+            },
+            options: {
+                watchTask: true,
+                server: {
+                    baseDir: "WebContent"
                 }
             }
         },
-		
-		sass: {
-			options: {
-				sourcemap: 'none'
-			},
-			dist: {
-				files: [
-					{ expand: true, cwd: 'sass/site', src: ['*.scss', '**/*.scss'], dest: 'WebContent/site/css', ext: '.css'},
-					{ expand: true, cwd: 'sass/blog', src: ['*.scss', '**/*.scss'], dest: 'WebContent/blog/css', ext: '.css'}
-				]
-			}
-		}
-		
-	});
-	
-	
-	//3. Load grunt plugings
-	grunt.loadNpmTasks('grunt-assemble');
-	grunt.loadNpmTasks('grunt-contrib-connect');
-	grunt.loadNpmTasks('grunt-contrib-sass');
-	
-	
-	//4. Custom or Default tasks
-	grunt.registerTask('default', ['connect']);
+
+        watch: {
+            options: {
+                livereload: true
+            },
+            html: {
+                files: 'app/assemble/**/*.hbs',
+                tasks: ['assemble']
+            },
+            sass: {
+                files: 'app/sass/**/*.scss',
+                tasks: ['sass']
+            },
+
+            copy: {
+                files: ['app/js/**/*.js', 'app/assets/images/*.*'],
+                tasks: ['copy']
+            }
+        },
+
+        copy: {
+            main: {
+                files: [
+                    // includes files within path
+                    {
+                        expand: true,
+												flatten: true,
+                        src: ['app/js/*.js', 'app/vendor/bootstrap3.3.7/js/*.js'],
+                        dest: 'WebContent/js/'
+                    },
+										{
+                        expand: true,
+												flatten: true,
+                        src: ['app/vendor/bootstrap3.3.7/css/*.css'],
+                        dest: 'WebContent/css/'
+                    },
+										{
+                        expand: true,
+												flatten: true,
+                        src: ['app/assets/images/*.*'],
+                        dest: 'WebContent/images/'
+                    },
+                ],
+            },
+        }
+
+        // connect: {
+        //     server: {
+        //         options: {
+        //             port: 9000, // The port on which the webserver will respond.
+        //             hostname: '*', // Default 'localhost'. Setting this to '*' will make the server accessible through IP. Useful for cross-device testing.
+        //             livereload: true,
+        //             open: true,
+        //             base: 'WebContent',
+        //             keepalive: true
+        //         }
+        //     }
+        // }
+
+    });
+
+
+    //3. Load grunt plugings
+    grunt.loadNpmTasks('grunt-assemble');
+    //grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-livereload');
+    grunt.loadNpmTasks('grunt-browser-sync');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+
+    //4. Custom or Default tasks
+    grunt.registerTask('default', ['assemble', 'sass', 'copy', 'browserSync', 'watch']);
 };
